50e02832cf7cd51fb19fc14e93379e376e258236
Leo Silveira <leosilveira74@outlook.com>
2024-09-18 15:21:42 -0300

    Subindo o projeto finalizado, foi adicionado o segundo desafio

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
 const numberInput = document.getElementById('number-question');
 const textInput = document.getElementById('text-question')
+const text = document.getElementById('.responseText')
 
 
-numberInput.addEventListener('change', () =>{
+numberInput.addEventListener('change', () => {
     const inputValue = numberInput.value
 
-    if(inputValue === '32'){
+    if (inputValue === '32') {
         alert('Parabéns, esse número pertence a sequência')
     } else {
         alert('Incorreto, tente novamente')
     }
 })
 
-// textInput.addEventListener('change', () => {
-//     const letterAExist = textInput.value;
-//     const letterA = 'a'
-//     console.log(letterAExist)
-// })
+textInput.addEventListener('change', () => {
+    let word = textInput.value
+
+    let amountA = (word.toLowerCase().match(/a/g) || []).length;
+
+    responseText.innerHTML = 
+    `
+        <p class="response">A letra 'a' aparece ${amountA} vezes na palavra "${word}".</p>
+    `
+})
